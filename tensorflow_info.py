2f7a37fa48e1e3e03826df700801e806399484e8
gauborg <gau.borg@gmail.com>
2020-06-18 20:40:20 -0400

    tensorflow info added

---
+++ tensorflow_info.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+# print tensorflow version
+print(__tf.version__)
+
+
+from tensorflow.device.client import device_lib
+device_lib.list_local_devices()
+
+! cat/proc/cpuinfo
+! cat/proc/meminfo
+
+
