6093519438098fa3893f309c7ba76769dacd0d21
Gaurav Borgaonkar <gau.borg@gmail.com>
2020-07-18 22:30:21 -0400

    Update and rename plot_histogram.py to 6_plot_histogram.py

--- 6_plot_histogram.py
+++ 6_plot_histogram.py
@@ -1,14 +1,14 @@
-"""
+'''
 ===========================
 Frontpage histogram example
 ===========================
 
 This example reproduces the frontpage histogram example.
-"""
+
+'''
 
 import matplotlib.pyplot as plt
 import numpy as np
-
 
 random_state = np.random.RandomState(19680801)
 X = random_state.randn(10000)
@@ -20,3 +20,4 @@ ax.plot(x, 1 / np.sqrt(2*np.pi) * np.exp(-(x**2)/2), linewidth=4)
 ax.set_xticks([])
 ax.set_yticks([])
 fig.savefig("histogram_frontpage.png", dpi=25)  # results in 160x120 px image
+
